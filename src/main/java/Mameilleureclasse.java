e68380af6cb923c341e3614e30e23e9153cfa425
Jiba Cyrille <jibacyrille@gmail.com>
2019-09-07 19:15:33 +0200

    Description du projet

---
+++ src/main/java/Mameilleureclasse.java
@@ -0,0 +1,5 @@
+public class Mameilleureclasse {
+    public static void main(String[] args) {
+        System.out.println("Voici ma deuxieme classe, la meilleure");
+    }
+}
--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,5 +1,6 @@
 public class demo {
     public static void main(String[] args) {
          System.out.println("Demo Git");
+        System.out.println("Projet demo pour apprendre git");
     }
 }
