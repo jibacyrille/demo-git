81078f2d1c35e40f54926c094fa006d0089f1213
Williams Penka <william@afreelancia.com>
2019-09-07 19:29:33 +0100

    Confirmation du code

--- src/main/java/Mameilleureclasse.java
+++ src/main/java/Mameilleureclasse.java
@@ -1,5 +1,6 @@
 public class Mameilleureclasse {
     public static void main(String[] args) {
-        System.out.println("Voici ma deuxieme classe, la meilleure");
+        System.out.println("Voici ma deuxieme classe, la meilleure ");
+        System.out.println("Je confirme le code");
     }
 }
